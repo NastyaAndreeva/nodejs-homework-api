7a59e7819b446d3eb8cdebf732fcb6eebe755b91
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2022-11-04 14:08:31 +0200

    fixed jimp

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -13,14 +13,15 @@ const updateAvatar = async (req, res) => {
 
   const extention = originalname.split(".").pop();
   const filename = `${_id}.${extention}`;
-  await Jimp.read(`${tempUpload}`, (err, fileName) => {
+  await Jimp.read(`${tempUpload}`, async (err, fileName) => {
     if (err) throw err;
     fileName
       .resize(250, 250) // resize
-      .write(`${avatarsDir}/${filename}`); // save
+      .write(`${tempUpload}`); // save
+    const resultUpload = path.join(avatarsDir, filename);
+    await fs.rename(tempUpload, resultUpload);
   });
-  // const resultUpload = path.join(avatarsDir, filenameJimp);
-  // await fs.rename(tempUpload, resultUpload);
+
   const avatarURL = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
 
