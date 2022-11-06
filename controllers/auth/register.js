01cb4dad49198ae07dee8022453c46458d16e32a
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2022-11-06 16:38:52 +0200

    fixed register

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,27 +1,24 @@
 const bcrypt = require("bcryptjs");
-const gravatar = require("gravatar");
 
 const { User } = require("../../models/user");
 
 const { RequestError } = require("../../helpers");
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { email, password, subscription } = req.body;
   const user = await User.findOne({ email });
   if (user) {
     throw RequestError(409, "Email in use");
   }
   const hashPassword = await bcrypt.hash(password, 10);
-  const avatarURL = gravatar.url(email);
   const result = await User.create({
-    name,
     email,
     password: hashPassword,
-    avatarURL,
+    subscription,
   });
   res.status(201).json({
-    name: result.name,
     email: result.email,
+    subscription: result.subscription,
   });
 };
 
